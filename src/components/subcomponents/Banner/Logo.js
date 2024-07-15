729a72486e73cf052d0e5f6207501701fdee4019
Julian <juliancoding30@gmail.com>
2024-07-14 17:17:58 -0700

    fixed height issue

--- src/components/subcomponents/Banner/Logo.js
+++ src/components/subcomponents/Banner/Logo.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
 
 export default function Logo({ image }) {
-    return (
-        <Image src={image.src} alt={image.alt} width={image.width} height={image.height} priority>
-        </Image>
-    )
+  return (
+    <Image
+      src={image.src}
+      alt={image.alt}
+      width={image.width}
+      height={image.height}
+      priority
+      className={`aspect-auto object-contain w-full min-h-3`}
+    ></Image>
+  );
 }
--- src/components/subcomponents/Banner/SlidingBanner.js
+++ src/components/subcomponents/Banner/SlidingBanner.js
@@ -3,7 +3,7 @@ import information from "../Information/BannerLogosInformation";
 
 export default function SlidingBanner() {
   return (
-    <div className="bg-black py-6 border border-l-0 border-r-0 border-t-2 border-b-2 border-white">
+    <div className="bg-black py-6 border border-l-0 border-r-0 border-t-2 border-b-2 border-white h-16">
       <div className="overflow-hidden">
         <div className="flex gap-16 sliding-banner">
           {information?.map((image, id) => (
